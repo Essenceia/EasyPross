b456b81fc9317ec5118a3a755a7fef2ba6bfa3b4
Igdrasill <couderc@edu.ece.fr>
2017-12-18 12:28:15 +0100

    J'ai enlevé les lock en trop

--- ModelController/src/Model/Register/DATAModel.java
+++ ModelController/src/Model/Register/DATAModel.java
@@ -8,15 +8,14 @@ public class DATAModel extends RegisterModel_Abstract{
      * @param fileName
      * @param block_size
      * @param block_count
-     * @param lock
      * @param data_in
      * @param data_out
      * @param id
      * @param type
      * @param description 
      */
-    public DATAModel(String fileName, int block_size, int block_count, String lock, boolean[] data_in, boolean[] data_out, int id, int type, String description) {
-        super(lock, data_in, data_out, id, type, description);
+    public DATAModel(String fileName, int block_size, int block_count,boolean[] data_in, boolean[] data_out, int id, int type, String description) {
+        super(data_in, data_out, id, type, description);
         this.fileName = fileName;
         this.block_size = block_size;
         this.block_count = block_count;
