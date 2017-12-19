63f47aeebae5b545763e769553ee6851e64c7fe1
Igdrasill <couderc@edu.ece.fr>
2017-12-19 15:22:21 +0100

    DEMUX

--- ModelController/src/Model/Logic/Unit/MUXModel.java
+++ ModelController/src/Model/Logic/Unit/MUXModel.java
@@ -6,7 +6,7 @@ public class MUXModel extends LogicUnitModel_Abstract {
 
     private int nb_bit_mux;
     private boolean[] instruct;
-    
+
     /**
      * Constructeur
      *
@@ -18,13 +18,12 @@ public class MUXModel extends LogicUnitModel_Abstract {
      * @param nb_mux
      * @param instruct
      */
-    public MUXModel(boolean[] data_in, boolean[] data_out, int id, int type, String description,int nb_mux, boolean[] instruct) {
+    public MUXModel(boolean[] data_in, boolean[] data_out, int id, int type, String description, int nb_mux, boolean[] instruct) {
         super(data_in, data_out, id, type, description);
-        this.nb_bit_mux=nb_mux;
+        this.nb_bit_mux = nb_mux;
         this.instruct = new boolean[3];
-        for(int i=0; i<3;i++)
-        {
-           this.instruct[i]=instruct[i]; 
+        for (int i = 0; i < 3; i++) {
+            this.instruct[i] = instruct[i];
         }
     }
 
@@ -36,75 +35,62 @@ public class MUXModel extends LogicUnitModel_Abstract {
     public void action() {
         get_incomming_data();
         boolean[] c = new boolean[3];
-        c[0]=instruct[0];
-        c[1]=instruct[1];
-        c[2]=instruct[2];
-        boolean [] data_out;
-        boolean [] e0;
-        boolean [] e1;
-        boolean [] e2;
-        boolean [] e3;
+        c[0] = instruct[0];
+        c[1] = instruct[1];
+        c[2] = instruct[2];
+        boolean[] data_out;
+        boolean[] e0;
+        boolean[] e1;
+        boolean[] e2;
+        boolean[] e3;
         switch (nb_bit_mux) {
             case 2:
                 e0 = new boolean[nb_bit_mux];
                 e1 = new boolean[nb_bit_mux];
                 data_out = new boolean[nb_bit_mux];
-                for(int i=3; i<nb_bit_mux+3; i++)
-                   {
-                       e0[i-3]=data_in[i];
-                   }
-                for(int i=3+nb_bit_mux; i<nb_bit_mux+3+nb_bit_mux; i++)
-                   {
-                       e1[i-3-nb_bit_mux]=data_in[i];
-                   }
-                if(c[0]== false && c[1]== false && c[2]== false)
-                {
-                   data_out=e0;
+                for (int i = 3; i < nb_bit_mux + 3; i++) {
+                    e0[i - 3] = data_in[i];
                 }
-                else 
-                {
-                   data_out=e1; 
+                for (int i = 3 + nb_bit_mux; i < nb_bit_mux + 3 + nb_bit_mux; i++) {
+                    e1[i - 3 - nb_bit_mux] = data_in[i];
+                }
+                if (c[0] == false && c[1] == false && c[2] == false) {
+                    data_out = e0;
+                } else {
+                    data_out = e1;
                 }
 
                 break;
-                
+
             case 4:
                 e0 = new boolean[nb_bit_mux];
                 e1 = new boolean[nb_bit_mux];
                 e2 = new boolean[nb_bit_mux];
                 e3 = new boolean[nb_bit_mux];
                 data_out = new boolean[nb_bit_mux];
-                for(int i=3; i<nb_bit_mux+3; i++)
-                   {
-                       e0[i-3]=data_in[i];
-                   }
-                for(int i=3+nb_bit_mux; i<nb_bit_mux+3+nb_bit_mux; i++)
-                   {
-                       e1[i-3-nb_bit_mux]=data_in[i];
-                   }
-                for(int i=3+(2*nb_bit_mux); i<3+(3*nb_bit_mux); i++)
-                   {
-                       e2[i-3-(2*nb_bit_mux)]=data_in[i];
-                   }
-                for(int i=3+(3*nb_bit_mux); i<3+(4*nb_bit_mux); i++)
-                   {
-                       e3[i-3-(3*nb_bit_mux)]=data_in[i];
-                   }
-                if(c[0]== false && c[1]== false && c[2]== false)
-                {
-                    data_out=e0;
+                for (int i = 3; i < nb_bit_mux + 3; i++) {
+                    e0[i - 3] = data_in[i];
                 }
-                if(c[0]== true && c[1]== false && c[2]== false)
-                {
-                    data_out=e1;
+                for (int i = 3 + nb_bit_mux; i < nb_bit_mux + 3 + nb_bit_mux; i++) {
+                    e1[i - 3 - nb_bit_mux] = data_in[i];
                 }
-                if(c[0]== false && c[1]== true && c[2]== false)
-                {
-                    data_out=e2;
+                for (int i = 3 + (2 * nb_bit_mux); i < 3 + (3 * nb_bit_mux); i++) {
+                    e2[i - 3 - (2 * nb_bit_mux)] = data_in[i];
                 }
-                if(c[0]== true && c[1]== true && c[2]== false)
-                {
-                    data_out=e3;
+                for (int i = 3 + (3 * nb_bit_mux); i < 3 + (4 * nb_bit_mux); i++) {
+                    e3[i - 3 - (3 * nb_bit_mux)] = data_in[i];
+                }
+                if (c[0] == false && c[1] == false && c[2] == false) {
+                    data_out = e0;
+                }
+                if (c[0] == true && c[1] == false && c[2] == false) {
+                    data_out = e1;
+                }
+                if (c[0] == false && c[1] == true && c[2] == false) {
+                    data_out = e2;
+                }
+                if (c[0] == true && c[1] == true && c[2] == false) {
+                    data_out = e3;
                 }
                 break;
             default:
