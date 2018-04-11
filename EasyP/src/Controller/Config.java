4496ef2181a1d48185ee3c208250e07c6ff3a16e
Julie Tiercelin <julie.tiercelin@edu.ece.fr>
2018-04-11 14:43:06 +0200

    fix only EasyPross modified

--- EasyP/src/Controller/Config.java
+++ EasyP/src/Controller/Config.java
@@ -1,10 +1,12 @@
 package Controller;
 
 public class Config {
+
     public static final String OS_SEPERATOR = System.getProperty("file.separator");
     public static final String PATH_BASE= System.getProperty("user.dir") + OS_SEPERATOR;
     public static final String PATH_FILE = PATH_BASE + "src" +System.getProperty("file.separator");
-
+    public static final double LINUX_MOUSE_OFFSET = 115;
+    public static final double WINDOWS_MOUSE_OFFSET = 109;
     /**
      * API Configurations
      */
